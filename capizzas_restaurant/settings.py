191858c38c99e40782d6c6ab44b26a4854e6d1d2
otaciliocesar <otaciliocesarsantos@gmail.com>
2025-07-07 11:24:36 -0300

    Ajuste tela cardapio, agora as fotos aparecem e tela login

--- capizzas_restaurant/settings.py
+++ capizzas_restaurant/settings.py
@@ -123,3 +123,7 @@ STATICFILES_DIRS = [BASE_DIR / "inventory" / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
