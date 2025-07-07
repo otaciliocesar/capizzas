e867ef76b25f0c37f693aa062c740288e0267e88
otaciliocesar <otaciliocesarsantos@gmail.com>
2025-07-07 11:24:36 -0300

    Ajuste tela cardapio, agora as fotos aparecem e tela login

--- capizzas_restaurant/urls.py
+++ capizzas_restaurant/urls.py
@@ -3,7 +3,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
-from . views import HomePageView, SobrePageView, LocalizacaoPageView, LoginPageView
+from . views import HomePageView, SobrePageView, LocalizacaoPageView
+from django.contrib.auth.views import LoginView, LogoutView
 
 app = 'capizzas_restaurant'
 
@@ -13,6 +14,7 @@ urlpatterns = [
     path('localizacao/', LocalizacaoPageView.as_view(), name='localizacao'),
     path('cardapio/', views.Cardapio, name='cardapio'),
     path('carrinho/', views.Carrinho, name='carrinho'),
-    path('login/', LoginPageView.as_view(), name='login'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('cadastropizza/add/', views.CadastroPizza, name='cadastropizza'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
