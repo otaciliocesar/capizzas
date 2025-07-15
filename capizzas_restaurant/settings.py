e253526bbf96bdbc5a4440b385dc1c0ffc46ca0b
otaciliocesar <otaciliocesarsantos@gmail.com>
2025-07-15 17:43:01 -0300

    Email confirmado

--- capizzas_restaurant/settings.py
+++ capizzas_restaurant/settings.py
@@ -134,9 +134,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'otaciliocesarsantos@gmail.com'
-EMAIL_HOST_PASSWORD = 'ppph ybbf yhxf ukey'  # não é sua senha normal
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
